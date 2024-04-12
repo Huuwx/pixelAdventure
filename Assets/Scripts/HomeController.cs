8acc5488fc1555908c581db1c71e8f6279ecf680
Huuwx <chumanhhuu2@gmail.com>
2024-04-12 15:42:57 +0700

    add setting btn

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HomeController : MonoBehaviour
 {
-    public string sceneName;
+    public GameObject panel;
+    public Button SettingBtn;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //panel.SetActive(false);
     }
 
 
@@ -23,5 +25,28 @@ public class HomeController : MonoBehaviour
     public void ExitBtn()
     {
         Application.Quit();
+    }
+
+    public void Setting()
+    {
+        panel.SetActive(true);
+        SettingBtn.enabled = false;
+    }
+
+    public void Resume()
+    {
+        panel.SetActive(false);
+        SettingBtn.enabled = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("Home");
     }
 }
