3e82089a39d3f3c5422b118496887f1c471c63fa
Huuwx <chumanhhuu2@gmail.com>
2024-05-17 22:32:29 +0700

    enemy chasing player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using static UnityEditor.PlayerSettings;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnemyController : MonoBehaviour
 {
 
-    public float Movdirection = -1f;
+    public float Movdirection = 0f;
     public float MoveSpeed = 1.5f;
+    private bool isMoving = false;
 
+    public Transform target;
     public GameObject PointCheck;
     public Vector2 sizePointCheck;
+    public GameObject CheckposPlayer;
+    public Vector2 sizeCheckPosPlayer;
 
     Rigidbody2D rigid;
     private Animator animator;
@@ -25,19 +32,20 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        CheckGround();
+        CheckDistance();
+        CheckPlayer();
         Movement();
-        //FlipEnemies();
+        FlipEnemies();
     }
 
     public void Movement()
     {
-        rigid.velocity = new Vector2(Movdirection*MoveSpeed, rigid.velocity.y);
+        rigid.velocity = new Vector2(Movdirection * MoveSpeed, rigid.velocity.y);
     }
 
     public void FlipEnemies()
@@ -50,8 +58,6 @@ public class EnemyController : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-
-        animator.SetBool("IsMoving", Movdirection != 0);
     }
 
     private void CheckGround()
@@ -83,22 +89,89 @@ public class EnemyController : MonoBehaviour
             {
                 if (Movdirection > 0)
                 {
-                    Movdirection = -1;
-                    transform.localScale = Vector3.one;
+                    Movdirection = -1f;
                     return;
                 }
                 else
                 {
-                    transform.localScale = new Vector3(-1, 1, 1);
-                    Movdirection = 1;
+                    Movdirection = 1f;
                     return;
                 }
             }
+            else if (collider.tag == "Player")
+            {
+                if (target.position.x < transform.position.x)
+                {
+                    Movdirection = -1f;
+                }
+                else
+                {
+                    Movdirection = 1f;
+                }
+                isMoving = true;
+                animator.SetBool("IsMoving", isMoving);
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+                rigid.MovePosition(temp);
+            }
+            else
+            {
+                Movdirection = 0f;
+                isMoving = false;
+                animator.SetBool("IsMoving", isMoving);
+            }
+        }
+    }
+
+    public void CheckDistance()
+    {
+        if(Mathf.Abs(transform.position.x - target.position.x) <= 2f)
+        {
+            Movdirection = 0f;
+            isMoving = false;
+            animator.SetBool("IsMoving", isMoving);
+        }
+    }
+
+    public void CheckPlayer()
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(CheckposPlayer.transform.position, sizeCheckPosPlayer, 0);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.tag == "Player")
+            {
+                Vector3 distance;
+                if (target.position.x < transform.position.x)
+                {
+                    Movdirection = -1f;
+                    distance = new Vector3(-2f, 0, 0);
+                }
+                else if(target.position.x > transform.position.x)
+                {
+                    Movdirection = 1f;
+                    distance = new Vector3(2f, 0, 0);
+                }
+                isMoving = true;
+                animator.SetBool("IsMoving", isMoving);
+                if (Mathf.Abs(transform.position.x - target.position.x) <= 2f)
+                {
+                    Movdirection = 0f;
+                    isMoving = false;
+                    animator.SetBool("IsMoving", isMoving);
+                }
+            }
+        }
+        if(colliders.Length == 1)
+        {
+            Movdirection = 0f;
+            isMoving = false;
+            animator.SetBool("IsMoving", isMoving);
+
         }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(PointCheck.transform.position, new Vector3(sizePointCheck.x, sizePointCheck.y, 1f));
+        Gizmos.DrawWireCube(CheckposPlayer.transform.position, new Vector3(sizeCheckPosPlayer.x, sizeCheckPosPlayer.y, 1f));
     }
 }
