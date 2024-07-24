b893c4a699179b7a9bd565a63ccc8cbe0ddea83d
Huuwx <chumanhhuu2@gmail.com>
2024-07-24 22:32:52 +0700

    web build

--- Assets/Scripts/General/GetValueFromDropDown.cs
+++ Assets/Scripts/General/GetValueFromDropDown.cs
@@ -6,13 +6,25 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown dropdown;
-    [SerializeField] public List<AudioClip> sourceList = SoundController.Instance.Sources;
+    [SerializeField] public List<AudioClip> sourceList;
+    //public List<TMP_Dropdown.OptionData> options;
+
+    // = SoundController.Instance.Sources
+
+    //private void Awake()
+    //{
+    //    dropdown.AddOptions(options);
+    //}
 
     public void GetDropdownValue()
     {
         int PickedEntryIndex = dropdown.value;
-        string selectionOption = dropdown.options[PickedEntryIndex].text;
-        Debug.Log(PickedEntryIndex);
+        //var selectionOption = dropdown.options[PickedEntryIndex];
         SoundController.Instance.PlayMusic(sourceList[PickedEntryIndex]);
+        //options[PickedEntryIndex] = options[0];
+        //options[0] = selectionOption;
+        //var music0 = sourceList[0];
+        //sourceList[0] = sourceList[PickedEntryIndex];
+        //sourceList[PickedEntryIndex] = music0;
     }
 }
